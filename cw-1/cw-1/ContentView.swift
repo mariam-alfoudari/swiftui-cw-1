983c60d188a0bc7879b6b9e74f0fb91588793608
mariam alfoudari <maf.alfoudari@gmail.com>
2020-12-25 18:57:54 +0300

    cw-1 done

---
+++ cw-1/cw-1/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  cw-1
+//
+//  Created by mariam alfoudari on 25/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+        Image("Background")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .ignoresSafeArea()
+        
+        VStack {
+            Image("Header")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+        
+             Spacer()
+            
+            Text("الحمد الله رب العالمين")
+            
+            Spacer()
+            
+            Image("bottom")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+            ContentView()
+        }
+    }
---
+++ cw-1/cw-1/cw_1App.swift
@@ -0,0 +1,17 @@
+//
+//  cw_1App.swift
+//  cw-1
+//
+//  Created by mariam alfoudari on 25/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct cw_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
