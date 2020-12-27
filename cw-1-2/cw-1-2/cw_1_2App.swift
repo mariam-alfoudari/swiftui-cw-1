53e3b519c1b38b5b45ad5a4c1ea321ae9e02ce24
mariam alfoudari <maf.alfoudari@gmail.com>
2020-12-27 04:58:11 +0300

    part1 dn

---
+++ cw-1-2/cw-1-2/cw_1_2App.swift
@@ -0,0 +1,17 @@
+//
+//  cw_1_2App.swift
+//  cw-1-2
+//
+//  Created by mariam alfoudari on 25/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct cw_1_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
